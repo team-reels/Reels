3d234146a764c154c9b84e0bfa60a633604e554e
EddisonSo <eddisonso@gmail.com>
2023-11-04 14:22:54 -0400

    added user.py

---
+++ api/domains/models/user.py
@@ -0,0 +1,33 @@
+from domain.models.base import Base
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, DateTime
+from sqlalchemy.sql import func
+from sqlalchemy import ForeignKey
+from uuid import uuid4
+from sqlalchemy.dialects.postgresql import UUID
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[UUID] = mapped_column(UUID(as_uuid=True),
+                                 primary_key=True,
+                                 default=uuid4)
+
+    username: Mapped[str] = mapped_column(String(64),
+                                          nullable=False)
+
+    biography: Mapped[str] = mapped_column(String(512),
+                                          nullable=False)
+
+    def get_username(self):
+        return self.username
+
+    def get_biography(self):
+        return self.biography
+
+    def set_username(self, username):
+        self.username = username
+
+    def set_biography(self, biography):
+        self.biography = biography
