5b7f3004ea62750302fa764f8749ff3979573776
EddisonSo <eddisonso@gmail.com>
2023-11-04 16:39:04 -0400

    added other blueprints

--- api/api.py
+++ api/api.py
@@ -1,10 +1,14 @@
 from flask import Flask
 from routes.user import user_blueprint
+from routes.comment import comment_blueprint
+from routes.catch import catch_blueprint
 
 
 def init_app():
     app = Flask(__name__)
     app.register_blueprint(user_blueprint)
+    app.register_blueprint(comment_blueprint)
+    app.register_blueprint(catch_blueprint)
     return app
 
 
