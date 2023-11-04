7326d0f874602647b942e9917c0f14d8e0a66b2e
EddisonSo <eddisonso@gmail.com>
2023-11-04 14:15:13 -0400

    finished catch model

--- api/domains/models/catch.py
+++ api/domains/models/catch.py
@@ -20,5 +20,38 @@ class Catch(Base):
     species: Mapped[str] = mapped_column(String(128), nullable=False)
     weight: Mapped[float] = mapped_column(nullable=False)
     size: Mapped[float] = mapped_column(nullable=False)
-    type: Mapped[int] = mapped_column(nullable=False)
+    fish_type: Mapped[int] = mapped_column(nullable=False)
     likes: Mapped[int] = mapped_column(nullable=False)
+
+    def get_post_time(self):
+        return self.post_time
+
+    def get_user_id(self):
+        return self.user_id
+
+    def get_species(self):
+        return self.species
+
+    def get_weight(self):
+        return self.weight
+
+    def get_size(self):
+        return self.size
+
+    def get_likes(self):
+        return self.likes
+
+    def set_species(self, species):
+        self.species = species
+
+    def set_weight(self, weight):
+        self.weight = weight
+
+    def set_size(self, size):
+        self.size = size
+
+    def set_type(self, fish_type):
+        self.fish_type = fish_type
+
+    def set_likes(self, likes):
+        self.likes = likes
