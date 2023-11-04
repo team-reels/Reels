6e7d22cb783f79efaab4c8f14be9dc5f9f0488c1
Issac <issac.zheng9869@gmail.com>
2023-11-04 17:36:19 -0400

    remove mui cos it's giving weird problems

--- reels-app/src/components/Nav.jsx
+++ reels-app/src/components/Nav.jsx
@@ -1,5 +1,4 @@
 import { useState } from 'react';
-import TextField from "@mui/material/TextField";
 import { FaCircleUser } from 'react-icons/fa6';
 import { Link } from 'react-router-dom';
 import '../styles/nav.scss';
@@ -12,7 +11,6 @@ function App() {
         <Link to='/'>
             <h1>Reels</h1> {/* replace with svg? */}
         </Link>
-        <TextField id="outlined-basic" label="Search" variant="outlined" />
         <Link to='/user'>
             <FaCircleUser size={45}/>
         </Link>
