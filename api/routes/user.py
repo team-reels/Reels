61d245ec0c04826d379871ae6e43ef52b43077a6
EddisonSo <eddisonso@gmail.com>
2023-11-04 15:40:02 -0400

    added user.py and defined add_user route

---
+++ api/routes/user.py
@@ -0,0 +1,27 @@
+from flask import Blueprint, jsonify, request
+from domain.repositories.user_repository import UserRepository
+from engine import engine
+from sqlalchemy.orm import Session
+
+user_api = Blueprint('user_api', __name__, url_prefix="/user_api")
+
+
+@user_api.route("/add_user", methods=["POST"])
+def add_user():
+    context = request.get_json()
+
+    if context.get("username") is None:
+        return jsonify({
+                "status": "failure",
+                "reason": "missing username"
+            })
+
+    username = context.get("username")
+
+    with Session(engine) as session:
+        user_repository = UserRepository(session)
+        new_user = user_repository.add_user(username)
+        return jsonify({
+                    "status": "success",
+                    "sensor id": new_user.id
+                })
