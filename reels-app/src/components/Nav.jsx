2f783e26401836e11ab48014f98b9f8781af128c
Issac <issac.zheng9869@gmail.com>
2023-11-04 16:23:20 -0400

    navbar, changed css to scss

--- reels-app/src/components/Nav.jsx
+++ reels-app/src/components/Nav.jsx
@@ -1,12 +1,16 @@
 import { useState } from 'react';
+import TextField from "@mui/material/TextField";
+import { FaCircleUser } from 'react-icons/fa';
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
-        Nav
-    </>
+    <div className='nav'>
+        <h1>Reels</h1> {/* replace with svg? */}
+        <TextField id="outlined-basic" label="Search" variant="outlined" />
+        <FaCircleUser />
+    </div>
   );
 };
 
