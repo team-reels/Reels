6dd511f3f8d4033ea0cbdfc94d5ff284a54ec936
EddisonSo <eddisonso@gmail.com>
2023-11-04 15:53:54 -0400

    modified engine.py to create tables

--- api/engine.py
+++ api/engine.py
@@ -3,10 +3,10 @@ from domains.models.base import Base
 from config import *
 
 #WARNING!!! DO NOT USE THIS IN PRODUCTION uwu. EVEN HAVING THIS HERE IS VERY BAD PRACTICE FOR OBVIOUS REASONS
-RESET_DB = False
+RESET_DB = True
 
 engine = create_engine(f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOSTNAME}:{DB_PORT}/{DB_NAME}",
                        echo=True)
 if RESET_DB:
     Base.metadata.drop_all(engine)
-
+    Base.metadata.create_all(engine)
