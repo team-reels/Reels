b8cfb8e73aad262f4780144b3047f1e50543edec
EddisonSo <eddisonso@gmail.com>
2023-11-04 13:57:43 -0400

    added fields for data

--- api/domains/models/catch.py
+++ api/domains/models/catch.py
@@ -2,12 +2,23 @@ from domain.models.base import Base
 from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy import String, DateTime
 from sqlalchemy.sql import func
+from sqlalchemy import ForeignKey
+from uuid import uuid4
+from sqlalchemy.dialects.postgresql import UUID
 
 
 class Catch(Base):
     __tablename__ = "catches"
 
-    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    id: Mapped[UUID] = mapped_column(UUID(as_uuid=True),
+                                 primary_key=True,
+                                 default=uuid4)
     post_time: Mapped[DateTime] = mapped_column(DateTime(timezone=True),
                                                 server_default=func.now())
-    
+    user_id: Mapped[UUID] = mapped_column(ForeignKey("user.id"),
+                                            nullable=False)
+    species: Mapped[str] = mapped_column(String(128), nullable=False)
+    weight: Mapped[float] = mapped_column(nullable=False)
+    size: Mapped[float] = mapped_column(nullable=False)
+    type: Mapped[int] = mapped_column(nullable=False)
+    likes: Mapped[int] = mapped_column(nullable=False)
