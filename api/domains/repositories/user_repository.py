27f514f6e4a17549df990211c4e82440c825dc8e
EddisonSo <eddisonso@gmail.com>
2023-11-04 14:53:03 -0400

    created user_repository.py

---
+++ api/domains/repositories/user_repository.py
@@ -0,0 +1,29 @@
+from sqlalchemy.orm import Session
+from domain.models.user import User
+
+
+def UserRepository():
+    db_session: Session
+
+    def __init__(self, db_session: Session):
+        self.session = db_session
+
+    """
+    Description: Adds new User object to be persisted
+    Arguments: new_user: User representing user to be added
+    Returns: User: User of added user data
+    """
+    def _add_user(self, new_user: User) -> User:
+        self.session.add(new_user)
+        self.session.commit()
+        return new_user
+
+    """
+    Description: Adds new User object to be persisted
+    Arguments: username: str of new user
+    Returns: User: User of added user data
+    """
+    def add_user(self, username) -> User:
+        new_user = User(username=username)
+        _add_user(new_user)
+        return new_user
