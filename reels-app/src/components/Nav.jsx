056e3a4c0490880f9d6e3ae0a9474d0f0ae74394
Issac <issac.zheng9869@gmail.com>
2023-11-04 13:41:40 -0400

    react init

---
+++ reels-app/src/components/Nav.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import './Nav.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+        Nav
+    </>
+  )
+}
+
+export default App
