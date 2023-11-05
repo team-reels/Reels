f30834d2844d9c63f9a9f42ca995813db92651f3
Aidan Haberman <aidanhaberman@icloud.com>
2023-11-04 22:59:20 -0400

    signup signin buttons work good :ok:

--- reels-app/src/components/auth/Signin.jsx
+++ reels-app/src/components/auth/Signin.jsx
@@ -1,7 +1,7 @@
-import { useEffect, useState } from 'react';
-import { signin } from '../../api/auth.js';
-import { Link } from 'react-router-dom';
-import '../../styles/auth.scss';
+import { useEffect, useState } from "react";
+import { signin } from "../../api/auth.js";
+import { Link } from "react-router-dom";
+import "../../styles/auth.scss";
 
 function Signin() {
 	const [checked, setChecked] = useState(false);
@@ -9,44 +9,69 @@ function Signin() {
 
 	useEffect(() => {
 		console.log(checked);
-	}, [checked])
+	}, [checked]);
 
-    const signIn = (e) => {
+	const signIn = (e) => {
 		e.preventDefault();
-		const email = document.getElementById('email').value;
-		const password = document.getElementById('password').value;
+		const email = document.getElementById("email").value;
+		const password = document.getElementById("password").value;
 		// continue with signin code here (import from api/auth.js and run code)
 		// navigate to '/'
-		signin(email, password)
+		signin(email, password);
 	};
 	return (
-		<div className='signin-container'>
-			<div className='signin-signup'>
-				<button className='signin'>
-					<Link to='/signin'>Sign In
-					</Link>
-				</button>
-				<button className='signup'>
-					<Link to='/signup'>Sign Up
-					</Link>
-				</button>
+		<div className="signin-container">
+			<div className="signin-signup">
+				<Link to="/signin">
+					<button className="signin">Sign In</button>
+				</Link>
+				<Link to="/signup">
+					<button className="signup">Sign Up</button>
+				</Link>
 			</div>
-			<form className='signin-form' onSubmit={signIn}>
+			<form className="signin-form" onSubmit={signIn}>
 				<label>
-					<input className='input' id='email' type='email' placeholder='email' />
-					</label>
-					<label>
-					<input className='input' id='password' type='password' placeholder='password' />
-					</label>
-					<button className='login-button' type='submit'>Login</button>
+					<input
+						className="input"
+						id="email"
+						type="email"
+						placeholder="email"
+					/>
+				</label>
 				<label>
-					<input type="checkbox" checked={checked} name="remember" onChange={() => setChecked(!checked)}/> Remember me
-					<Link to='/forgot' style={{textAlign:'right', display:'inline-block', float:'right'}}>Forgot Password</Link>
+					<input
+						className="input"
+						id="password"
+						type="password"
+						placeholder="password"
+					/>
+				</label>
+				<button className="login-button" type="submit">
+					Login
+				</button>
+				<label>
+					<input
+						type="checkbox"
+						checked={checked}
+						name="remember"
+						onChange={() => setChecked(!checked)}
+					/>{" "}
+					Remember me
+					<Link
+						to="/forgot"
+						style={{
+							textAlign: "right",
+							display: "inline-block",
+							float: "right",
+						}}
+					>
+						Forgot Password
+					</Link>
 				</label>
 				{/* <div class="divider">or</div> */}
 			</form>
 		</div>
 	);
-};
+}
 
 export default Signin;
--- reels-app/src/components/auth/Signup.jsx
+++ reels-app/src/components/auth/Signup.jsx
@@ -1,53 +1,59 @@
-import { useEffect, useState } from 'react';
-import { signup } from '../../api/auth.js';
-import { Link } from 'react-router-dom';
-import '../../styles/auth.scss';
+import { useEffect, useState } from "react";
+import { signup } from "../../api/auth.js";
+import { Link } from "react-router-dom";
+import "../../styles/auth.scss";
 
 function SignUp() {
-    const signUp = (e) => {
-        e.preventDefault();
-        const email = document.getElementById('email').value;
-        const username = document.getElementById('username').value;
-        const password = document.getElementById('password').value;
-        // continue with signin code here (import from api/auth.js and run code)
-        // navigate to '/'
-    };
+	const signUp = (e) => {
+		e.preventDefault();
+		const email = document.getElementById("email").value;
+		const username = document.getElementById("username").value;
+		const password = document.getElementById("password").value;
+		// continue with signin code here (import from api/auth.js and run code)
+		// navigate to '/'
+	};
 
-    return (
-        <div className="signin-container">
-            <button className="signin">Sign In</button>
-            <button className="signup">Sign Up</button>
-            <form className="signin-form" onSubmit={signup}>
-                <label>
-                    <input
-                        className="input"
-                        id="email"
-                        type="email"
-                        placeholder="email"
-                    />
-                </label>
-                <label>
-                    <input
-                        className="input"
-                        id="username"
-                        type="username"
-                        placeholder="username"
-                    />
-                </label>
-                <label>
-                    <input
-                        className="input"
-                        id="password"
-                        type="password"
-                        placeholder="password"
-                    />
-                </label>
-                <button className="login-button" type="submit">
-                    Login
-                </button>
-            </form>
-        </div>
-    );
+	return (
+		<div className="signin-container">
+			<div className="signin-signup">
+				<Link to="/signin">
+					<button className="signin">Sign In</button>
+				</Link>
+				<Link to="/signup">
+					<button className="signup">Sign Up</button>
+				</Link>
+			</div>
+			<form className="signin-form" onSubmit={signup}>
+				<label>
+					<input
+						className="input"
+						id="email"
+						type="email"
+						placeholder="email"
+					/>
+				</label>
+				<label>
+					<input
+						className="input"
+						id="username"
+						type="username"
+						placeholder="username"
+					/>
+				</label>
+				<label>
+					<input
+						className="input"
+						id="password"
+						type="password"
+						placeholder="password"
+					/>
+				</label>
+				<button className="login-button" type="submit">
+					Login
+				</button>
+			</form>
+		</div>
+	);
 }
 
 export default SignUp;
