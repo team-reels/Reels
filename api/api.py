3a92a34cbcfa4314f114894cd37ff7b96ff6a58a
EddisonSo <eddisonso@gmail.com>
2023-11-05 07:05:08 -0500

    added cors

--- api/api.py
+++ api/api.py
@@ -2,6 +2,7 @@ from flask import Flask
 from routes.user import user_blueprint
 from routes.comment import comment_blueprint
 from routes.catch import catch_blueprint
+from flask_cors import CORS
 
 
 def init_app():
@@ -9,4 +10,5 @@ def init_app():
     app.register_blueprint(user_blueprint)
     app.register_blueprint(comment_blueprint)
     app.register_blueprint(catch_blueprint)
+    cors = CORS(app)
     return app
