aadbcb0e082f02cb4ba0989aa716df6632912674
Aidan Haberman <aidanhaberman@icloud.com>
2023-11-04 22:48:59 -0400

    signup

--- reels-app/src/components/auth/Signup.jsx
+++ reels-app/src/components/auth/Signup.jsx
@@ -1,12 +1,53 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { signup } from '../../api/auth.js';
+import { Link } from 'react-router-dom';
 import '../../styles/auth.scss';
 
 function SignUp() {
-  return (
-    <>
-		SignUp
-    </>
-  );
-};
+    const signUp = (e) => {
+        e.preventDefault();
+        const email = document.getElementById('email').value;
+        const username = document.getElementById('username').value;
+        const password = document.getElementById('password').value;
+        // continue with signin code here (import from api/auth.js and run code)
+        // navigate to '/'
+    };
+
+    return (
+        <div className="signin-container">
+            <button className="signin">Sign In</button>
+            <button className="signup">Sign Up</button>
+            <form className="signin-form" onSubmit={signup}>
+                <label>
+                    <input
+                        className="input"
+                        id="email"
+                        type="email"
+                        placeholder="email"
+                    />
+                </label>
+                <label>
+                    <input
+                        className="input"
+                        id="username"
+                        type="username"
+                        placeholder="username"
+                    />
+                </label>
+                <label>
+                    <input
+                        className="input"
+                        id="password"
+                        type="password"
+                        placeholder="password"
+                    />
+                </label>
+                <button className="login-button" type="submit">
+                    Login
+                </button>
+            </form>
+        </div>
+    );
+}
 
 export default SignUp;
