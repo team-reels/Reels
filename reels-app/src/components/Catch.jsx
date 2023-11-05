d758878d094c6a77c9b211e6120819e1a7f6bb4a
Issac <issac.zheng9869@gmail.com>
2023-11-05 00:23:48 -0400

    some stuff

--- reels-app/src/components/Catch.jsx
+++ reels-app/src/components/Catch.jsx
@@ -1,13 +1,36 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { AuthContext } from '../contexts/AuthContext';
+import { useParams } from 'react-router-dom';
+import useAxios from '../hooks/useAxios';
+
+// NOTE: halting this page; unneeded for MVP
 
 function Catch() {
-  const [count, setCount] = useState(0);
+	// const { catchData, setCatchData } = useAxios('link');
+	const catchData = {
+		id: '1',
+		species: 'fish',
+		weight: 5,
+		size: 5,
+		type: 'fish',
+		likes: 5
+	};
+	const params = useParams(); 
+	const catchId = params.catchId;
 
-  return (
-    <div>
-        Catch
-    </div>
-  );
+	return (
+		<div className='content-container catch'>
+			{/* <div className='buttons'>
+				<button className='next'>Next</button>
+				<button className='prev'>Prev</button>
+			</div> */}
+			<div>
+				
+			</div>
+			{catchData.id}
+			
+		</div>
+	);
 };
 
 export default Catch;
