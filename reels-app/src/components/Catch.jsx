af13d3e9f9d853b76cf1c31b5de44c04787f8571
Britney Yang <byang.careers@gmail.com>
2023-11-05 06:19:20 -0500

    made basic catch page

--- reels-app/src/components/Catch.jsx
+++ reels-app/src/components/Catch.jsx
@@ -1,12 +1,36 @@
 import { useState } from 'react';
+import dev_fish from '../assets/dev_fish.jpg';
+
 
 function Catch() {
+  const image={dev_fish}, species='fish', weight=5, size=5, type='fish', likes=5;
   const [count, setCount] = useState(0);
 
   return (
-    <div>
-        Catch
-    </div>
+		<div className='add-container'>
+      <img src={image} alt='Fish'></img>
+				<form className='form-container'>
+					<label>
+						Species:
+						{species}
+					</label>
+					<label>
+						Weight:
+						{weight}
+					</label>
+					<label>
+						Length:
+						{size}
+					</label>
+					<label>
+						Bio:
+						<input
+							className="input"
+							id="bio"
+						/>
+					</label>
+				</form>
+		</div>
   );
 };
 
