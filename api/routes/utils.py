ff953c2532ebc07fee3cc116b9c31d6435161304
EddisonSo <eddisonso@gmail.com>
2023-11-08 15:19:42 -0500

    added check query params decorator

--- api/routes/utils.py
+++ api/routes/utils.py
@@ -1,6 +1,7 @@
 from functools import wraps
 from flask import request, jsonify
 
+
 def require_json_params(params):
     def decorator(func):
         @wraps(func)
@@ -10,6 +11,20 @@ def require_json_params(params):
                 if context.get(i) is None:
                     return jsonify({
                             "status": "failure", "reason": f"missing {i}"
+                    })
+            return func()
+        return returned_func
+    return decorator
+
+def require_query_params(args):
+    def decorator(func):
+        @wraps(func)
+        def returned_func(*args, **kwargs):
+            request_args = request.args
+            for i in args:
+                if request_args.get(i) is None:
+                    return jsonify({
+                            "status": "failure", "reason": f"missing argument {i}"
                     })
             return func()
         return returned_func
