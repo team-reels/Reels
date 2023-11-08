42e2be02015d8a41a871f643d3f2d484ab78ba17
EddisonSo <eddisonso@gmail.com>
2023-11-08 15:17:09 -0500

    used new check args decorator

--- api/routes/catch.py
+++ api/routes/catch.py
@@ -10,108 +10,48 @@ catch_blueprint = Blueprint('catches', __name__, url_prefix="/catch_api")
 CORS(catch_blueprint)
 
 
-
-@catch_blueprint.route("/add_catch", methods=["POST"])
-@require_json_params(["uid", "species", "weight", "size"])
+@catch_blueprint.route("/add_catch", methods=["PUT"])
+@require_json_params(["uid", "species", "weight", "size", "iid"])
 def add_catch():
     context = request.get_json()
 
     uid = context.get("uid")
     species = context.get("species")
     weight = context.get("weight")
-    size = context.get("weight")
-    image_id = context.get("image_id")
-
-    with Session(engine) as session:
-        catch_repository = CatchRepository(session)
-        new_catch = catch_repository.add_catch(user_id=uid, species=species,
-                                              weight=weight, size=size, image_id=image_id)
-        return jsonify({
-                    "status": "success",
-                    "catch_id": new_catch.id,
-                })
-
-@catch_blueprint.route("/edit_catch", methods=["POST"])
-def edit_catch():
-    context = request.get_json()
-
-    if context.get("cid") is None:
-        return jsonify({
-                "status": "failure", "reason": "missing id"
-            })
-
-    if context.get("uid") is None:
-        return jsonify({
-                "status": "failure", "reason": "missing user_id"
-            })
-
-    if context.get("species") is None:
-        return jsonify({
-                "status": "failure",
-                "reason": "missing species"
-            })
-
-    if context.get("weight") is None:
-        return jsonify({
-                "status": "failure",
-                "reason": "missing weight"
-            })
-
-    if context.get("size") is None:
-        return jsonify({
-                "status": "failure",
-                "reason": "missing size"
-            })
-
-    id = context.get("cid")
-    user_id = context.get("uid")
-    species = context.get("species")
-    weight = context.get("weight")
     size = context.get("size")
+    image_id = context.get("iid")
 
     with Session(engine) as session:
         catch_repository = CatchRepository(session)
-        catch = catch_repository.get_catch(id)
-
-        if str(catch.user_id) != user_id:
+        try:
+            new_catch = catch_repository.add_catch(user_id=uid, species=species,
+                                               weight=weight, size=size,
+                                               image_id=image_id)
             return jsonify({
-                    "status": "failure",
-                    "reason": "incorrect user",
-                    "user_id": user_id,
-                    "catch.user_id": catch.user_id
-                })
-
-        catch_repository.edit_catch(id=id, species=species, weight=weight, size=size)
-        return jsonify({
-                    "status": "success",
-                    "catch id": catch.id
-                })
+                        "status": "success",
+                        "catch_id": new_catch.id,
+                    })
+        except:
+            result = jsonify({
+                        "status": "failure",
+                        "catch_id": new_catch.id,
+                    })
+            return result, 400
 
 
-@catch_blueprint.route("/get_catch", methods=["POSt"])
+@catch_blueprint.route("/get_catch", methods=["GET"])
+@require_json_params(["cid"])
 def get_catch():
     context = request.get_json()
 
-    if context.get("cid") is None:
-        return jsonify({
-                "status": "failure",
-                "reason": "missing cid"
-            })
-
-    id = context.get("cid")
+    cid = context.get("cid")
 
     with Session(engine) as session:
         catch_repository = CatchRepository(session)
-        catch = catch_repository.get_catch(id)
+        catch = catch_repository.get_catch(cid)
         return jsonify({
                     "status": "success",
-                    "cid": catch.id,
-                    "uid": catch.user_id,
-                    "species": catch.species,
-                    "weight": catch.weight,
-                    "size": catch.size,
-                    "type": catch.type,
-                    "likes": catch.likes
+                    "catch": Catch.to_JSON(catch)
                 })
 
 
