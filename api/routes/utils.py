9689c89f958067f21846bbc938ecb0452bfb51f3
EddisonSo <eddisonso@gmail.com>
2023-11-08 15:09:07 -0500

    fixed decorator to check args

--- api/routes/utils.py
+++ api/routes/utils.py
@@ -5,11 +5,12 @@ def require_json_params(params):
     def decorator(func):
         @wraps(func)
         def returned_func(*args, **kwargs):
+            context = request.get_json()
             for i in params:
-                if request.json and request.json.get(i) is None:
+                if context.get(i) is None:
                     return jsonify({
                             "status": "failure", "reason": f"missing {i}"
                     })
             return func()
         return returned_func
-    return decorator 
+    return decorator
