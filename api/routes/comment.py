85b7b230076b258b794eb9ac01c15f6fa42abe38
EddisonSo <eddisonso@gmail.com>
2023-11-08 21:17:07 -0500

    added error handling

--- api/routes/comment.py
+++ api/routes/comment.py
@@ -4,17 +4,19 @@ from engine import engine
 from sqlalchemy.orm import Session
 from flask_cors import CORS
 from routes.utils import require_json_params, require_query_params
+from domains.repositories.repo_exceptions import IdExistsException, IdMissingException, UsernameExistsException
 
 comment_blueprint = Blueprint('comments', __name__, url_prefix="/comment_api")
 CORS(comment_blueprint)
 
 
-@comment_blueprint.route("/add_comment", methods=["GET"])
-@require_query_params(["uid", "cid", "comment"])
+@comment_blueprint.route("/add_comment", methods=["POST"])
+@require_json_params(["uid", "cid", "comment"])
 def add_comment():
-    uid = request.args.get("uid")
-    cid = request.args.get("cid")
-    comment = request.args.get("comment")
+    context = request.get_json
+    uid = context.get("uid")
+    cid = context.get("cid")
+    comment = context.get("comment")
 
     with Session(engine) as session:
         comment_repository = CommentRepository(session)
@@ -30,12 +32,18 @@ def add_comment():
 @comment_blueprint.route("/get_comment", methods=["POST"])
 @require_query_params(["cid"])
 def get_comment():
-    cid = request.json.get("cid")
+    cid = request.args.get("cid")
 
     with Session(engine) as session:
-        comment_repository = CommentRepository(session)
-        comment = comment_repository.get_comment(cid=cid)
-        return jsonify({
-                    "status": "success",
-                    "comment": comment.to_JSON()
-                })
+        try:
+            comment_repository = CommentRepository(session)
+            comment = comment_repository.get_comment(cid=cid)
+        except IdExistsException:
+            return jsonify({
+                        "status": "failure",
+                        "reason": f"cid {cid} already exists"
+                    })
+    return jsonify({
+        "status": "success",
+        "comment": comment.get_JSON()
+        })
